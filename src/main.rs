163c0845c9313de6162acfc478e4edb7fccff92a
Wendell Smith <wendellwsmith@gmail.com>
2015-03-15 11:50:02 -0400

    Added threading

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use std::process::{Command,Stdio};
 use std::sync::mpsc::sync_channel;
 use std::thread;
 
+enum Line {
+    Child(String),
+    Stdin(String)
+}
+
 fn main() {
-    let stdin = std::io::stdin();
-    let stdin_buf = stdin.lock();
-    
     let comm = Command::new("sh")
         .arg("-c")
         .arg("for i in $(seq 1 3); do sleep 1; echo line $i; done")
@@ -17,10 +19,37 @@ fn main() {
     
     let child_buf = std::io::BufReader::new(comm.stdout.unwrap());
     
-    for line_result in child_buf.lines() {
+    let (tx_stdin, rx) = sync_channel(1);
+    let tx_child = tx_stdin.clone();
+    
+    thread::spawn(move|| {
+        for line_result in child_buf.lines() {
+            let line = match line_result {
+                Ok(line) => Line::Child(line),
+                Err(e) => {println!("Child error: {}", e); break;}
+            };
+            tx_child.send(line).unwrap();
+        }
+    });
+    
+    
+    thread::spawn(move|| {
+        let stdin = std::io::stdin();
+        let stdin_buf = stdin.lock();
+        
+        for line_result in stdin_buf.lines() {
+            let line = match line_result {
+                Ok(line) => Line::Stdin(line),
+                Err(e) => {println!("Error: {}", e); break;}
+            };
+            tx_stdin.send(line).unwrap();
+        }
+    });
+    
+    for line_result in rx.iter() {
         match line_result {
-            Ok(line) => println!("Read line {}", line),
-            Err(e) => {println!("Error: {}", e); break;}
+            Line::Child(line) => println!("CHILD: {}", line),
+            Line::Stdin(line) => println!("STDIN: {}", line),
         }
     }
 }
