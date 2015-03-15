6f400a2a87654eda802e9a0c3cc89b697ae2e3ef
Wendell Smith <wendellwsmith@gmail.com>
2015-03-15 10:18:55 -0400

    Working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![feature(io)]
+
+use std::io::BufReadExt;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let stdin_buf = stdin.lock();
+    
+    for line_result in stdin_buf.lines() {
+        match line_result {
+            Ok(line) => println!("Read line {}", line),
+            _ => break
+        }
+    }
+    
+    // loop {
+    //     match stdin_buf.read_line() {
+    //         Ok(line) => println!("Read line {}", line),
+    //         _ => break
+    //     }
+    // }
 }
